555e1ee28c079cd9a79a37886ddbcd32f891ce87
Gerseneck <wang14240@gmail.com>
2024-09-24 17:05:52 -0400

    feat: pages model and basic editorjs functionality

--- app/javascript/controllers/editor_controller.js
+++ app/javascript/controllers/editor_controller.js
@@ -1,19 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
-import { EditorJS } from "@editorjs/editorjs"
-import { Paragraph } from "@editorjs/paragraph"
-import { Header } from "@editorjs/header"
-import { List } from "@editorjs/list"
-import { Quote } from "@editorjs/quote"
-import { Delimiter } from "@editorjs/delimiter"
-import { LinkTool } from "@editorjs/link"
-import { ImageTool } from "@editorjs/image"
-import { AttachesTool } from "@editorjs/attaches"
+import EditorJS from "@editorjs/editorjs"
+import Paragraph from "@editorjs/paragraph"
+import Header from "@editorjs/header"
+import List from "@editorjs/list"
+import Quote from "@editorjs/quote"
+import Delimiter from "@editorjs/delimiter"
+import LinkTool from "@editorjs/link"
+import ImageTool from "@editorjs/image"
+import AttachesTool from "@editorjs/attaches"
 
 // Connects to data-controller="editor"
 export default class extends Controller {
+  static targets = ["content"]
+
   connect() {
-    const editor = new EditorJS({
-      holder: 'editorjs',
+    this.content_field = document.getElementById("content_hidden")
+    this.editor = new EditorJS({
+      holder: this.contentTarget,
+      data: this.getInitalContent(),
       tools: {
         paragraph: {
           class: Paragraph,
@@ -25,5 +29,19 @@ export default class extends Controller {
         delimiter: Delimiter
       }
     })
+
+    this.element.addEventListener("submit", this.saveData.bind(this))
+  }
+
+  getInitalContent() {
+    return this.content_field.value ? JSON.parse(this.content_field.value) : {}
+  }
+  
+  async saveData(event) {
+    event.preventDefault()
+    
+    const data = await this.editor.save()
+    this.content_field.value = JSON.stringify(data)
+    this.element.submit()
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :pages
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
   get "dashboard", to: "welcome#dashboard"
