d8c0ea0865b805adb2b78eee1275a5c635d0c90f
Gerseneck <wang14240@gmail.com>
2024-09-23 20:48:19 -0400

    wip: dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    if admin?
+      dashboard_path
+    else
+      root_path
+    end
+  end
+
+  def admin?
+    current_user&.admin
+  end
+
+  def check_for_user
+    redirect_to root_path, notice: 'You must be logged in to do that' unless current_user
+  end
+
+  def check_for_admin
+    redirect_to root_path, notice: 'You are not allowed to do that' unless admin?
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :remember_me) }
+  end
 end
