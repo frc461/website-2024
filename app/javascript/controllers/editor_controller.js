c7b7f6d6123715e165e09f65e26105168b36a3bb
leo-minton <leo.a.minton+1@gmail.com>
2024-10-05 16:15:08 -0400

    basic setup

--- app/javascript/controllers/editor_controller.js
+++ app/javascript/controllers/editor_controller.js
@@ -1,18 +1,4 @@
-import { Controller } from "@hotwired/stimulus"
-import EditorJS from "@editorjs/editorjs"
-import Paragraph from "@editorjs/paragraph"
-import Header from "@editorjs/header"
-import NestedList from "@editorjs/nested-list"
-import Quote from "@editorjs/quote"
-import Delimiter from "@editorjs/delimiter"
-import Table from "@editorjs/table"
-import Underline from "@editorjs/underline"
-import LinkTool from "@editorjs/link"
-import ImageTool from "@editorjs/image"
-import AttachesTool from "@editorjs/attaches"
-import Alert from "editorjs-alert"
-
-import { Card } from "../card";
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="editor"
 export default class extends Controller {
@@ -21,38 +7,9 @@ export default class extends Controller {
   connect() {
     this.content_field = document.getElementById("content_hidden");
 
-    let tools = {
-      paragraph: {
-        class: Paragraph,
-        inlineToolbar: true,
-      },
-      header: Header,
-      list: {
-        class: NestedList,
-        inlineToolbar: true,
-        config: {
-          defaultStyle: "ordered",
-        },
-      },
-      quote: Quote,
-      delimiter: Delimiter,
-      table: Table,
-      underline: Underline,
-      alert: Alert,
-      card: {
-        class: Card,
-        inlineToolbar: true,
-        config: {
-          tools() { return tools }
-        },
-      },
-    };
-    this.editor = new EditorJS({
-      holder: this.contentTarget,
-      data: this.getInitalContent(),
-      tools: tools,
-      minHeight: 50,
-    });
+    this.editor = new Quill("#content", {
+      theme: 'snow'
+    }); 
 
     this.element.addEventListener("submit", this.saveData.bind(this))
   }
