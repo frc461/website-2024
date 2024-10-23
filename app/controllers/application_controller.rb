a95e1ad4d93e17c9a682a3e34e2c6258a33ce3ee
Gerseneck <wang14240@gmail.com>
2024-10-23 09:49:08 -0400

    fix: dashboard index path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
   def after_sign_in_path_for(resource)
-    dashboard_path
+    dashboard_index_path
   end
 
   def admin?
