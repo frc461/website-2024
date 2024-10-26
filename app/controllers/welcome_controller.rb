ff68136e85d99a4ae558632aa52cf9033d8b27ff
Gerseneck <wang14240@gmail.com>
2024-10-26 10:29:23 -0400

    init: category and other pages (stuff on navbar)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,19 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def about
+  end
+
+  def history
+  end
+
+  def news
+  end
+
+  def resources
+  end
+
+  def safety
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :page_assets, only: %i[ index show destroy ] do
     post :upload_image, on: :collection
   end
-  get "search" => "search#index"
+  get "search", to: "search#index"
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -13,6 +13,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "welcome#index"
+  get "about_us", to: "welcome#about"
+  get "history", to: "welcome#history"
+  get "news", to: "welcome#news"
+  get "resources", to: "welcome#resources"
+  get "safety", to: "welcome#safety"
 
   resources :dashboard, only: :index do
     get :settings, on: :collection
