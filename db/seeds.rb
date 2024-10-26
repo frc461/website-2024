573bc1c4d9fc2ff5da095854a552466c89be5016
Gerseneck <wang14240@gmail.com>
2024-10-26 16:26:09 -0400

    feat: implemented seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,20 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+include ApplicationHelper
+
 ["history", "news", "resources"].each do |category_name|
   PageCategory.find_or_create_by!(name: category_name)
+end
+
+Dir["db/seeds_json/*/*"].each do |file|
+  name = file.delete_prefix("db/seeds_json/").chomp(".json").split('/')
+  unless Page.find_by_title(name[1].titleize)
+    Page.create!(
+      title: name[1].titleize,
+      content: File.read(file),
+      html_cache: ApplicationHelper.render_content(File.read(file)),
+      page_category_id: PageCategory.friendly.find(name[0]).id
+    )
+  end
 end
