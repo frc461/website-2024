560b1f5b18b656f238f35b3fca9a5e4e69fa03d6
Gerseneck <wang14240@gmail.com>
2024-10-28 21:02:14 -0400

    feat: newsletter seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,9 +16,10 @@ end
 
 Dir["db/seeds_json/*/*"].each do |file|
   name = file.delete_prefix("db/seeds_json/").chomp(".json").split('/')
-  unless Page.find_by_title(name[1].titleize)
+  title = name[1].split('-').length == 1 ? name[1].titleize : name[1].split('-')[-1].titleize
+  unless Page.find_by_title(title)
     Page.create!(
-      title: name[1].titleize,
+      title: title,
       content: File.read(file),
       html_cache: ApplicationHelper.render_content(File.read(file)),
       page_category_id: PageCategory.friendly.find(name[0]).id
