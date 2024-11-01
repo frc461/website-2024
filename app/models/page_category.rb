7c83894d55119851ff88187c3240f771e1c69b6b
Gerseneck <wang14240@gmail.com>
2024-10-31 20:33:50 -0400

    feat: simple validations

--- app/models/page_category.rb
+++ app/models/page_category.rb
@@ -2,4 +2,7 @@ class PageCategory < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
   has_many :pages, dependent: :destroy
+
+  validates_presence_of :name
+  validates_uniqueness_of :name
 end
