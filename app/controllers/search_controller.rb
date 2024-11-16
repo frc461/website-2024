857cfcbf919d2c16a113d4459ddcac202164029f
Gerseneck <wang14240@gmail.com>
2024-11-15 19:02:13 -0500

    feat: search for page templates

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
   def index
     pages = policy_scope(Page).ransack(title_or_html_cache_cont: params[:q]).result
     users = policy_scope(User).ransack(name_cont: params[:q]).result
-    @results = pages + users
+    page_templates = policy_scope(PageTemplate).ransack(name_cont_or_template_cont: params[:q]).result
+    @results = pages + users + page_templates
   end
 end
--- app/policies/page_template_policy.rb
+++ app/policies/page_template_policy.rb
@@ -8,7 +8,11 @@ class PageTemplatePolicy < ApplicationPolicy
   class Scope < ApplicationPolicy::Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      scope.all
+      if user
+        scope.all
+      else
+        scope.none
+      end
     end
   end
 end
